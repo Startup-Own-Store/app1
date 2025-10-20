fae6b8e9e63adc02bc11365aec1730fc7a446d29
Ajay-at-github <73772218103@ksrct.net>
2025-10-20 11:52:18 +0530

    Updated files

--- SupabaseClient.js
+++ SupabaseClient.js
@@ -1,8 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
+import auth from '@react-native-firebase/auth';
 
-const supabaseUrl = "https://ubadjmwfiujrrsqcliyt.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViYWRqbXdmaXVqcnJzcWNsaXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0MDc1MjcsImV4cCI6MjA3MDk4MzUyN30.NhuZSMKrMJwZLiyOzVs-D5s_-rsh3kBGKamJILorBFY";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(
+  'https://ubadjmwfiujrrsqcliyt.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViYWRqbXdmaXVqcnJzcWNsaXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0MDc1MjcsImV4cCI6MjA3MDk4MzUyN30.NhuZSMKrMJwZLiyOzVs-D5s_-rsh3kBGKamJILorBFY',
+  {
+    accessToken: async () => {
+      const user = auth().currentUser;
+      return user ? await user.getIdToken() : null;
+    },
+  }
+);
 
 export default supabase;
