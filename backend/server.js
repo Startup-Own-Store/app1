50e3e629fdd638360ce4f12d0127aa23e4985462
Ajay-at-github <73772218103@ksrct.net>
2025-11-10 12:58:13 +0530

    Updated backend folder

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const createOrder = require('./app/api/create-order');
-
+const cors = require('cors');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 
+app.use(cors());
 app.use(bodyParser.json());
-
-app.post('/app/api/create-order', createOrder);
 
 app.listen(PORT, () => {
   console.log(`Backend server running on http://localhost:${PORT}`);
